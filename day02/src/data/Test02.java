07c4d9c0fb5fed5a56a3e3c5288eb13a19470773
juyeonjo21 <merry2j@naver.com>
2023-06-20 12:40:33 +0900

    중국집 음식 계산 문제 풀이

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("자장면 한 그릇 7000원");
+		System.out.println("짬뽕 한 그릇 8000원");
+		System.out.println("자장면 두 그릇과 짬뽕 세 그릇의 총 가격은 "
+		+ ( (7000*2) + (8000*3) ) + "원 입니다.");
+		//잘못된 예시 System.out.println("38000");
+		
+		//잘못된 예시2 System.out.println(14000+24000);
+		
+		//바른예시 System.out.println(7000*2 + 8000*3);
+		
+	}
+
+}
