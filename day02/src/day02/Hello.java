f3ad237f33258e25f999628925ef48ce18db038e
juyeonjo21 <merry2j@naver.com>
2023-06-20 10:49:50 +0900

    Hello 다시 생성

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,9 @@
+package day02; //이 파일의 위치
+
+public class Hello { //Hello 클래스(=파일명)
+	public static void main(String[] args) {  //Main 메소드(고정)
+		System.out.println("Hello!"); //출력명령
+		System.out.println(100); //숫자는 따옴표 안 씀
+	}
+
+}
