44d5c62bce5b94477ca95230c2fc628d1335b9f2
juyeonjo21 <merry2j@naver.com>
2023-06-20 11:15:06 +0900

    첫 테스트

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//따옴표를 쓰는 기준은 "계산"을 하는가이다.
+		//숫자이지만 고유번호같은 숫자들(전화번호,비밀번호 등)은 따옴표 안에 기입
+		System.out.println("2023-06-20"); //글자 그대로를 출력
+		System.out.println(2023-06-20); //숫자를 출력(계산식)
+		
+	}
+}
