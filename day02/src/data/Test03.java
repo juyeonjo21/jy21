d184bb1e507f7aab04f570b256a2913e0768c2f8
juyeonjo21 <merry2j@naver.com>
2023-06-20 12:52:22 +0900

    변수 사용1

---
+++ day02/src/data/Test03.java
@@ -0,0 +1,18 @@
+package data;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//변수(Variable) = 데이터 기억장치
+		//이름은 중복해서 지정할 수 없음
+		int a = 7000*2; //7000*2 를 계산해서 a라는 이름으로 저장(대입)한다는 뜻
+		int b = 8000*3;
+		
+		System.out.println(a); //값을 구할 것이기 떄문에 따옴표는 빼고 기입
+		//a에 저장된 데이터를 출력
+		System.out.println(b);
+		//b에 저장된 데이터를 출력
+		System.out.println(a + b);
+		
+	}
+
+}
