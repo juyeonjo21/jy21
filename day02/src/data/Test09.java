7057dc0ec8c0a8106479e967449340ed3d0013a6
juyeonjo21 <merry2j@naver.com>
2023-06-20 15:56:55 +0900

    계산법 실습(시간,날짜)

---
+++ day02/src/data/Test09.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Test09 {
+	public static void main(String[] args) {
+		
+		//한 달에 200만원씩 30년을 모으면 얼마인가?
+		
+		//입력
+		int money = 2000000;
+		int year = 30;
+		
+		//계산
+		int month = year * 12;
+		int total = money * month;
+		
+		//출력
+		System.out.println(total);
+		
+		
+	}
+
+}
