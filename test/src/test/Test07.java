97abd9679db0036f79ad856c266278e346fb8884
juyeonjo21 <merry2j@naver.com>
2023-06-21 20:41:38 +0900

    day03 과제 제출 파일

---
+++ test/src/test/Test07.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Test07 {
+
+	public static void main(String[] args) { 
+		//윤년 계산기 만들기 test
+		//4의 배수인 해는 윤년이다
+		//100의 배수인 해는 4의 배수일지라도 윤년이 아니다
+		//400의 배수인 해는 100의 배수일지라도 윤년이다
+
+				int year = 2002;
+				
+				boolean fourYear = year % 4 == 0 ;   //4의 배수
+		    	boolean  hundredYear = year % 100 != 0 ; // 100의 배수
+				boolean  fourHundredYear = year % 400 == 0 ; //400의 배수
+			
+				boolean leapYear = ( fourYear && hundredYear ) || fourHundredYear ;
+
+				System.out.println(leapYear);
+
+	}
+
+}
