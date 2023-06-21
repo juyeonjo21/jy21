1be62b09a8355a05e4dcf65c6eea7f2c9562775e
juyeonjo21 <merry2j@naver.com>
2023-06-21 11:11:15 +0900

    할인 금액 구하기 실습 문제 & 풀이

---
+++ day03/src/data/Test_15.java
@@ -0,0 +1,25 @@
+package data;
+
+public class Test_15 {
+	public static void main(String[] args) {
+		//kh여행사에서는 다음과 같이 프로모션을 진행 중입니다.
+		//국내 어디든 한 사람당 1박 10만원
+		//추가로 25% 할인까지 챙겨가세요.
+		//(Q)성인 3명이 7일동안 여행갈 때의 예상 경비를 출력하세요.
+		
+		//입력
+		int people = 3;
+		int days = 7;
+		
+		//계산
+		int price = people * days * 100000; 
+		
+		int discount = price * 25 / 100;  //75%
+		int result = price - discount;
+		
+		System.out.println(price);
+		System.out.println(discount);
+		System.out.println(result);
+		
+	}
+}
