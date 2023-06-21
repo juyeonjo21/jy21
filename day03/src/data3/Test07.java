9afbaab0103a77ccbce24c60d3577868fe5c50cc
juyeonjo21 <merry2j@naver.com>
2023-06-21 18:14:45 +0900

    day03과제

---
+++ day03/src/data3/Test07.java
@@ -0,0 +1,18 @@
+package data3;
+
+public class Test07 {
+	public static void main(String[] args) {
+		//윤년 계산기 만들기 
+		// 4의 배수는 윤년o , 100의 배수는 윤년x , 400의 배수는 윤년o
+		
+		int year = 2002;
+
+		//4의 배수인 해는 윤년이다
+		//100의 배수인 해는 4의 배수일지라도 윤년이 아니다
+		//400의 배수인 해는 100의 배수일지라도 윤년이다
+		
+	
+	
+	
+	}
+}
