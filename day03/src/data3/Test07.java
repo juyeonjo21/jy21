2fc06e9afc6031cea1c1176627f81c011442af36
juyeonjo21 <merry2j@naver.com>
2023-06-22 09:35:01 +0900

    day03 과제
    윤년 계산기 만들기

--- day03/src/data3/Test07.java
+++ day03/src/data3/Test07.java
@@ -2,17 +2,20 @@ package data3;
 
 public class Test07 {
 	public static void main(String[] args) {
-		//윤년 계산기 만들기 
-		// 4의 배수는 윤년o , 100의 배수는 윤년x , 400의 배수는 윤년o
-		
-		int year = 2002;
-
+		//윤년 계산기 만들기 test
 		//4의 배수인 해는 윤년이다
 		//100의 배수인 해는 4의 배수일지라도 윤년이 아니다
 		//400의 배수인 해는 100의 배수일지라도 윤년이다
+
+		int year = 2002;
 		
+		boolean fourYear = year % 4 == 0 ;   //4의 배수
+		boolean  hundredYear = year % 100 != 0 ; // 100의 배수
+		boolean  fourHundredYear = year % 400 == 0 ; //400의 배수
 	
-	
+		boolean leapYear =  fourYear && hundredYear  || fourHundredYear ;
+
+		System.out.println(leapYear);
 	
 	}
 }
