2e8104ef968c276270345411596974e4cd4de7ff
juyeonjo21 <merry2j@naver.com>
2023-06-23 10:39:07 +0900

    if-else if -else 구문 예제

--- day05/src/condition2/Test02.java
+++ day05/src/condition2/Test02.java
@@ -1,6 +1,6 @@
 package condition2;
 
-public class Test01 {
+public class Test02 {
 	public static void main(String[] args) {
 		//그룹 조건
 		//-3가지 이상 중 하나의 경우를 선택하여 실행하는 상황
@@ -8,29 +8,22 @@ public class Test01 {
 		//-8시 이전에 일어나면 환승은 해야 하지만 비교적 빠른 지하철을 탑승
 		//-9시 이전에 일어나면 무조건 택시를 탑승
 		
-		int hour = 8;
+		//if-else if-else 구문
+		int hour = 11;
 		
 		if(hour <= 7 ) {
-			System.out.println("버스 도착 시간을 알아봐줄게");
-		}
-		
-		else {
-			if(hour <= 8) {
+			System.out.println("버스 도착 시간을 알아봐줄게"); //*시작은 if여야함
+	     	}
+		else if(hour <= 8) {
 			System.out.println("지하철 시간을 알아봐줄게");
 			}
-			
-		else {
-			if(hour <= 9 ) {
+		else if(hour <= 9 ) {
 			System.out.println("택시를 불러줄게");
 			}
-			
 	    else {
-			System.out.println("끝났어 그냥 천천히 가");
-		}
-				
-				
-	}	
-		}
-}	
-}		
-
+			System.out.println("끝났어 그냥 천천히 가"); //*마지막은 else여야함. 조건은 없음. 나머지라서 
+		    }	
+		
+	 }	
+}
+		
