bca19326e6f1427f72ba5c6aa820dbbcbea3e137
juyeonjo21 <merry2j@naver.com>
2023-06-23 17:27:18 +0900

    for문 예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)문
+		//-[Q] Hello를 화면에 10번 출력하세요.
+		for(int i = 0; i < 20; i++) {
+			System.out.println("Hello");
+		}
+	}
+
+}
