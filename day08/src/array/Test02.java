764c14ec0380e759189eb96cdc7aa925dd11791d
juyeonjo21 <merry2j@naver.com>
2023-06-28 09:59:54 +0900

    array(배열) 필요성과 사용법

---
+++ day08/src/array/Test02.java
@@ -0,0 +1,31 @@
+package array;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		//배열(array)
+		//-여러 개의 같은 종류 데이터를 저장할 수 있는 공간
+		
+		//생성
+		//a(리모컨) ------->*시작지점[0][0][0] 00번째 ->X / 시작지점 기준으로 거리 순으로 판단 (ex.자)
+		int[] a = new int[3];
+//             -> int[] = 정수 여러 개의 거치공간 *new가 나오면 참조형(리모컨)
+
+//		a = 10; //error(컴파일 에러)
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+//		a[3] = 40; //실행하면 error(런타임 에러)
+		
+		
+		
+//		System.out.println(a); //a=리모컨 , 리모컨 정보 출력
+		System.out.println(a[0]); //a가 보는 +0지점 데이터
+		System.out.println(a[1]); //a가 보는 +1지점 데이터
+		System.out.println(a[2]); //a가 보는 +2지점 데이터
+//		System.out.println(a[3]); //없음(error) 
+
+
+	}
+
+}
