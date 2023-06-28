917048d57cc458abdc370a51953eeda0fdc804c0
juyeonjo21 <merry2j@naver.com>
2023-06-28 10:18:03 +0900

    array 배열 문제 풀이
    소수점 & 문자

---
+++ day08/src/array/Test04.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Test04 {
+	public static void main(String[] args) {
+		//[2] 자바, 파이썬, 루비를 저장하기 위한 배열을 생성하고 저장한 뒤 출력
+		//생성
+		//data ----> [ null ][ null ][ null ] *null이 초기값이다.
+		String[] data = new String[3];
+		
+		//입력
+		data[0] = "자바";
+		data[1] =  "파이썬";
+		data[2] = "루비";
+
+		//출력
+		System.out.println(data[0]);
+		System.out.println(data[1]);
+		System.out.println(data[2]);
+
+	}
+
+}
