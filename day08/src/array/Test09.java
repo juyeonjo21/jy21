4aa2fbbe410072896ccb47583b0399cb8b339a1b
juyeonjo21 <merry2j@naver.com>
2023-06-28 15:47:01 +0900

    로또 추첨 문제 풀이

---
+++ day08/src/array/Test09.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Random;
+
+public class Test09 {
+
+	public static void main(String[] args) {
+		//[Q] 로또 문제 1000번 추첨 / 1~45 몇번 나왔느냐 / 가장 많이 나온 번호 찾기
+		Random r = new Random();
+		
+		int[] countList = new int[45]; //변수 6개를 대신하는 것
+
+
+		for(int i = 0; i < 1000; i++ ) {
+			int lotto = r.nextInt(45) +1;
+			countList[lotto-1]++; // ex : n = [n-1] ++ 이게 공식임
+		}	
+		for(int i = 0; i < countList.length;  i++ ) {
+			System.out.println( (i+1) +"의 나온 횟수 : " + countList[i] );
+		
+	}
+
+		}
+}
+
