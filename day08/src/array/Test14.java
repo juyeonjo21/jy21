24531f09ccf745e8625673c8f995f7d5ac0bf49e
juyeonjo21 <merry2j@naver.com>
2023-06-29 09:59:38 +0900

    day08 배열 셔플 문제 풀이

--- day08/src/array/Test14.java
+++ day08/src/array/Test14.java
@@ -1,4 +1,5 @@
 package array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Test14 {
@@ -6,25 +7,28 @@ public class Test14 {
 	public static void main(String[] args) {
 		Random r = new Random();
 		//[Q] 카드섞기 랜덤
-		int[] data = new int[] {30, 50, 20,10, 40}; // 0-1-2-3-4
+		//[1]모든 자리는 1번씩 다른 위치와 바뀌어야 한다
+		//[2]어디랑 섞일지 대상 위치는 모르며 같은 위치일 수도 있다
+		//[3]랜덤으로 정해진 위치와 현재 위치의 데이터를 바꾸도록 구현
+		int[] data = new int[] {30, 50, 20, 10, 40}; // 0-1-2-3-4
 		
-		
-		int index = 0;
 		
 		for(int i = 0; i < data.length; i ++) {
-			int card = r.nextInt(data[i]);
-		    index = r.nextInt();
-				
-			
-			
-			System.out.println(i);	
-			}
+		    int randomIndex = r.nextInt(data.length); //배열 위치
+		    
+		    int card = data[i];
+		    data[i] = data[randomIndex];
+		    data[randomIndex] = card;
 		
-		
-		/*	int maxIndex = 0; //0번 위치가 가장 크다고 가정!
-		for(int i = 1; i < data.length; i ++) { //1부터 쓰기
-			if(data[maxIndex] < data[i] ) { 
-				//위치를 갈아엎는다.  * 더큰숫자 = 맥스보다 큰 배열 값
-				maxIndex = i;*/
+		    System.out.println("카드넘버 : " + data[i] );
+		    System.out.println("섞인 위치 : " + randomIndex); 
+		    
 		}
-}
+	        System.out.println("랜덤 카드 배열 : " + Arrays.toString(data)); 
+		}
+	}
+
+
+
+
+
---
+++ day08/src/array/Test14_1.java
@@ -0,0 +1,45 @@
+package array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test14_1 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		//[Q] 카드섞기 문제 풀이*
+		
+		//데이터 준비
+		int[] data = new int[] {30, 50, 20, 10, 40}; // 0-1-2-3-4
+		
+		//셔플(뒤섞기)
+		//-같은자리 제거
+		//-같은 자리가 나오면 다시 뽑아라
+		//-같은 자리가 나오면 하던 작업을 무효화시켜라
+		for(int i = 0; i < data.length; i++ ) {
+		//[i] <---->[0~4]
+		int index = r.nextInt(data.length);
+		
+		if(i ==  index ) { //같은 자리가 나왔다면
+			i--;
+			continue;
+		}
+		
+		
+		int backup = data[i];  //data [  ] 이 자리만 채우면 위치를 계속 바꿀 수 있음
+		data[i] = data[index];
+		data[index] = backup;
+		}	
+		
+		//출력
+		for(int i = 0; i < data.length; i++ ) {
+			System.out.println(data[i]);
+			
+		}
+		
+		}
+	}
+
+
+
+
+
