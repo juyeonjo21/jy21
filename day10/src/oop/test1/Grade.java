bd9050f1ce2cdb0244eee3afb2e40e824df59f90
juyeonjo21 <merry2j@naver.com>
2023-06-30 10:34:17 +0900

    day10 객체지향 프로그래밍 프로젝트 생성

---
+++ day10/src/oop/test1/Grade.java
@@ -0,0 +1,13 @@
+package oop.test1;
+
+//시험 성적 정보를 자바에게 설명하기 위해 만드는 도구(=클래스)
+public class Grade {
+	//시험 성적 정보의 구성요소 데이터를 정의(=멤버 필드)
+	String name;
+	int korean;
+	int english;
+	int math;         // 이 변수들 4개를 하나로 묶어서 Grade를 만든다(클래스)
+	
+	}
+
+
