80d27a39ba7a7c500c719a493873762c27e283c6
juyeonjo21 <merry2j@naver.com>
2023-07-04 10:50:50 +0900

    public 사용

--- day12/src/oop/moditier3/Student.java
+++ day12/src/oop/moditier3/Student.java
@@ -1,5 +1,26 @@
 package oop.moditier3;
 
+//필드 - private / 클래스 - public
 public class Student {
+	private String name;
+	private int score;
 
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public Student() {}  //기본 생성자
 }
--- day12/src/oop/moditier4/Test01.java
+++ day12/src/oop/moditier4/Test01.java
@@ -1,5 +1,19 @@
 package oop.moditier4;
 
-public class Test01 {
+import oop.moditier3.Student; //추가해줘야함. 패키지가 다르니까
 
+public class Test01 {
+	public static void main(String[] args) {
+		//Student의 객체(인스턴스) 생성
+		//-자바의 기본 접근 제한은 패키지 범위로 설정되어 있다.
+		//-패키지에 없는 것은 모른다고 간주
+		//-import를 통해 알려줘서 해결 가능
+		//-단, import는 public일 때만 가능
+		Student a = new Student();
+		
+		//a.name = "피카츄" -> 안됨
+		a.setName("피카츄");
+		a.setScore(100);
+	}
+	
 }
