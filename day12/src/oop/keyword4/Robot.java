b352ff2f7b66c9f46a43c198b1fa40e16b6497da
juyeonjo21 <merry2j@naver.com>
2023-07-04 14:21:43 +0900

    참조형 데이터(ex.배열) 메소드 처리

--- day12/src/oop/keyword4/Robot.java
+++ day12/src/oop/keyword4/Robot.java
@@ -1,5 +1,7 @@
 package oop.keyword4;
 
+import java.util.Random;
+
 public class Robot {
 	
 	//[1] 제곱 계산 기능
@@ -31,8 +33,69 @@ public class Robot {
 	}
 	public static double circle(double radius) {
 		return square(radius)* 3.14; //오버로딩 가능
-
+	}
 	
+	//[4] 배열을 선택 정렬하는 메소드
+	public static void sort(int[] data) {
+		//선택정렬
+		for(int k = 0; k < data.length -1; k++) {
+	     	int minIndex = k;//k번 위치의 값이 가장 작다고 생각
+		for(int i = k+1; i < data.length; i ++ ) {//뒤에 있는 데이터와 비교하여
+			if(data[minIndex] > data[i]) { // 더 작은 값이 있다면
+				minIndex = i; //교체!
+		}
+	}
+		//	System.out.println("minIndex = " + minIndex );
+			
+			int backup = data[minIndex];
+			data[minIndex] = data[k];
+			data[k] = backup;
+		}
+	}
+		
+	//[5] 배열을 뒤집는 메소드(reverse)
+	public static void reverse(int[] data) {
+		int left = 0; //왼쪽 위치
+		int right = data.length - 1; 			  //오른쪽 위치 *규칙 잘 기억.
+
+		for(int  i = 0; i < data.length/ 2 ; i++ ) {			
+			int backup = data[left];
+			data[left] = data[right];
+			data[right] = backup;
+			
+			left++;
+			right--;
+		}
+
+	}	
+	
+	//[6] 배열을 무작위로 섞는 메소드(shuffle)
+	public static void shuffle(int[] data) {
+		Random r = new Random();
+		for(int i = 0; i < data.length; i++ ) {
+			//[i] <---->[0~4]
+			int index = r.nextInt(data.length);
+			
+			if(i ==  index ) { //같은 자리가 나왔다면
+				i--; //무효화시킨다
+				continue;
+			}
+			
+			
+			int backup = data[i];  //data [  ] 이 자리만 채우면 위치를 계속 바꿀 수 있음
+			data[i] = data[index];
+			data[index] = backup;
+			
+		}	
 }
+	//[7] 배열을 출력하는 메소드
+	public static void print (int[] data) {
+		for(int i = 0; i < data.length; i ++ ) {
+			System.out.print(data[i]);
+			System.out.print("\t");
+		}
+		System.out.println();
+	}
+
 }	
 
--- day12/src/oop/keyword4/Test01.java
+++ day12/src/oop/keyword4/Test01.java
@@ -12,6 +12,17 @@ public class Test01 {
 		
 		float c = Robot.circle(5);
 		System.out.println("원의 넓이 = " + c);
+		
+		
+		//(주의) 참조형 데이터는 리모컨만 전달하면 외부에서 직접 제어 가능
+		int[] data = new int[] {30, 50, 20, 10, 40 };
+		Robot.sort(data); //data=리모컨 / 리모컨 전달(call - by - refernce)
+		Robot.print(data);
+		
+		Robot.reverse(data);
+		Robot.print(data);
+		
+		Robot.shuffle(data);
+		Robot.print(data);
 	}
-
 }
