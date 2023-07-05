037f161fbe6b0808463344c9979c54a7c065a8ea
juyeonjo21 <merry2j@naver.com>
2023-07-05 19:18:10 +0900

    다형성 문제

---
+++ day13/src/oop/inherit8/Interaction.java
@@ -0,0 +1,13 @@
+package oop.inherit8;
+
+//두 개의 캐릭터로 서로간의 전투(상호작용)를 하도록 처리하는 클래스
+// - ex : 거래 / 전투 / ...
+public  class Interaction {
+
+	//전투기능 - 메소드
+	public static void battle(Warrior a, Warrior b ) {//  3  3  3    = 9개
+
+	}
+}
+
+
