6cd629f692d04cf6ed7ef42cccb34002c950bf0e
juyeonjo21 <merry2j@naver.com>
2023-07-05 16:24:02 +0900

    추상클래스의 상속

---
+++ day13/src/oop/inherit7/Galaxy.java
@@ -0,0 +1,9 @@
+package oop.inherit7;
+
+public abstract class Galaxy extends Mobile {
+
+	public Galaxy(String number, String color) {
+		super(number, color);
+	}
+	public abstract void samsungPay();
+}
