ae97ad5f218fc15c8ef51be2db5b702219d3b2e3
juyeonjo21 <merry2j@naver.com>
2023-07-05 09:56:59 +0900

    상속 설명

--- day13/src/oop/inherit1/Iphone14.java
+++ day13/src/oop/inherit1/Iphone14.java
@@ -1,20 +1,20 @@
 package oop.inherit1;
 
-public class Iphone13 {
+public class Iphone14 {
 	//필드 - 데이터 저장
 	private String number; //전화번호
 	private String color; //색상
 	private int price; //가격
 	
 	//메소드 - 기능
-	// setter, getter 생략
+	//setter, getter 생략
 	public void call() {
 		System.out.println("전화 걸기");
 	}
 	public void camera() {
 		System.out.println("사진 찍기");
 	}
-	public void siri() {
-		System.out.println("시리야");
+	public void facetime() {
+		System.out.println("영상통화");
 	}
 }
--- day13/src/oop/inherit1/Test01.java
+++ day13/src/oop/inherit1/Test01.java
@@ -3,6 +3,16 @@ package oop.inherit1;
 public class Test01 {
 
 	public static void main(String[] args) {
+		Iphone13 a = new Iphone13();
+		a.call();
+		a.camera();
+		a.siri();
+		
+		Iphone14 b = new Iphone14();
+		b.call();
+		b.camera();
+		b.facetime();
+		
 
 	}
 
