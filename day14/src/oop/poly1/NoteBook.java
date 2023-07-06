ea598753f9e873940086bfdefca72e3067ce2698
juyeonjo21 <merry2j@naver.com>
2023-07-06 11:12:31 +0900

    다형성 (업캐스팅) 문제 풀이

---
+++ day14/src/oop/poly1/NoteBook.java
@@ -0,0 +1,9 @@
+package oop.poly1;
+
+public abstract class  NoteBook {
+
+	public abstract void power();
+	public abstract void video();
+	public abstract  void typing(); 
+	
+	}
---
+++ day14/src/oop/poly1/Test01.java
@@ -0,0 +1,44 @@
+package oop.poly1;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		int type = 2;
+		int action = 1;
+
+		NoteBook notebook;
+		if(type == 1) {
+			notebook = new MacBook(); //맥북 -> 노트북 (업캐스팅)
+		}
+		else {
+			notebook = new GalaxyBook(); //갤럭시북 -> 노트북 (업캐스팅)
+		}
+		
+		//notebook에 들어간 객체의 기능을 실행
+		if(action == 1) {
+			notebook.power();
+		}
+		else if(action == 2) {
+			notebook.video();
+		}
+		else {
+			notebook.typing();
+		}
+		
+		
+		
+		
+/*		NoteBook a = new NoteBook("맥북");
+		a.power();
+		a.video();
+		a.typing();
+		
+		NoteBook b = new NoteBook("갤럭시북");
+		b.power();
+		b.video();
+		b.typing();
+		
+*/		
+	}
+
+}
