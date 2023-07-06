641d69a3d5301473963d70b37bf7a390eb0a7fee
juyeonjo21 <merry2j@naver.com>
2023-07-06 17:57:44 +0900

    문자열 로그인 문제 풀이

---
+++ day14/src/api/lang/string/Test02.java
@@ -0,0 +1,22 @@
+package api.lang.string;
+
+import java.util.Scanner;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("아이디 : ");
+		String a = sc.next();
+		System.out.println("비밀번호 : ");
+		String b = sc.next();
+		
+		String ID = new String("khacademy");
+		ID = ID.toLowerCase();
+		String PW = new String("student1234");
+		
+		//System.out.println(ID.lo);
+		
+	}
+
+}
