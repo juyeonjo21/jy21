3bf20d8d18f03e54c32e787ab7d3f11b7a973732
juyeonjo21 <merry2j@naver.com>
2023-07-06 10:18:58 +0900

    다형성 문제 풀이

--- day13/src/oop/inherit8/Test03.java
+++ day13/src/oop/inherit8/Test03.java
@@ -2,7 +2,8 @@ package oop.inherit8;
 
 public class Test03 {
 	public static void main(String[] args) {
-
+		//객체를 업캐스팅하여 생성
+		//= 상속 관계에서 특정 클래스로 대충 묶어서 표현하고 싶을 때
 		
 		Player a = new Warrior("전사1");   //a(리모컨) = 레퍼런스
 		//Magician a = new Magician("테스터1");
