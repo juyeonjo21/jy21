237c4692ada6ee892a0988e7914c4038e66e23f8
juyeonjo21 <merry2j@naver.com>
2023-07-10 22:24:39 +0900

    day16 과제

---
+++ test2/src/api/exception/Test06.java
@@ -0,0 +1,49 @@
+package api.exception;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+			//만나이 계산기 과제
+		
+			try { //플랜A
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.println("출생년-월 (yyyy-mm) : " );
+				String input = sc.next();
+				sc.close();
+				
+				if(input.length() < 7 ) {
+					throw new Exception("입력 값이 너무 짧습니다.");
+				}
+				
+				int birthYear = Integer.parseInt(input.substring(0,4)); //user 출생 년도
+				if(birthYear < 1900 ) {
+					throw new Exception("년도는 1900년도 초과만 입력 가능합니다.");
+				}
+				
+				int birthMonth = Integer.parseInt(input.substring(5, 7)); //user 출생 월
+				if(birthMonth < 1 || birthMonth > 12 ) {
+					throw new Exception("월은 1~12월만 입력 가능합니다.");
+				}
+				
+				Calendar cal = Calendar.getInstance();
+				int year = 	cal.get(Calendar.YEAR); //현재 년도
+				int month = cal.get(Calendar.MONTH) +1 ; //현재 달
+				//나이 계산
+				int currentAge = year - birthYear +1 ; //현재나이
+				int americanAge = year - birthYear -1;  //만 나이
+				if(month >= birthMonth ) {
+					americanAge += 1;  // 생일 달이거나, 지났다면 = 만나이 +1
+	  }
+				
+				System.out.println("현재 나이 : " + currentAge + " 세" );
+				System.out.println("만 나이 : " + americanAge + " 세" );
+}		
+			catch(Exception e ) {
+					System.err.println("입력 오류");
+	   }	
+	}
+}
