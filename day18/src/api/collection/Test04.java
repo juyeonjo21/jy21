79a9734ce7476050265bb8aea0b199164937c12a
juyeonjo21 <merry2j@naver.com>
2023-07-12 15:09:27 +0900

    로또번호 list 문제

---
+++ day18/src/api/collection/Test04.java
@@ -0,0 +1,30 @@
+package api.collection;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Test04 {
+	public static void main(String[] args) {
+		//[Q] 이번주 로또 당첨 예상번호를 추첨하는 프로그램을 구현하고 싶습니다.
+		//보너스 번호를 제외한 6개의 '1~45' 범위 내에서 중복없는 숫자를 추첨한 뒤 저장소에 저장 후 출력
+		//int, long과 같은 원시형은 Generic Type을 사용할 수 없다(=null때문)
+		//int -> integer
+		Random r = new Random();
+		
+		ArrayList<Integer> lotto = new ArrayList<>();
+
+	//	lotto.add(r.nextInt(45)+1);
+
+			for(int i = 0; i <  6 ; i++ ) {
+				int num = r.nextInt(45)+1;
+			if(lotto.contains(num)) {
+				i--; //재추첨
+			}
+			else {
+				lotto.add(num);	
+			}
+		}
+			System.out.println(lotto);
+			
+		}
+	}
