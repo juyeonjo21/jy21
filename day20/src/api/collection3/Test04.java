21a46b191dd2f576503057f1d68a4ae91b76e20f
juyeonjo21 <merry2j@naver.com>
2023-07-14 10:26:49 +0900

    Map 전체 출력 예제

---
+++ day20/src/api/collection3/Test04.java
@@ -0,0 +1,23 @@
+package api.collection3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Test04 {
+	public static void main(String[] args) {
+		//Map의 전체출력
+		//Map<String, Integer> vote = new HashMap<>();
+		Map<String, Integer> vote = Map.of("아이유",5,"피카츄",2,"영탁",4,"라이츄",3);
+		
+		//Map에서 key만 보면 Set의 형태이다.
+		// -> 일단 key만 추출하여 출력하고, value는 그때 그때 가져와서 사용
+		Set<String> names = vote.keySet(); //vote의 key만 추출하라!
+		for(String name : names) {
+			int count = vote.get(name);
+			System.out.println("이름 = " + name + ", 득표 수 = " + count );
+		}
+		
+	}
+
+}
