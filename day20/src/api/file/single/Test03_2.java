4e003a5e797f1bc2fe5e958e7ab516439ce1fee1
juyeonjo21 <merry2j@naver.com>
2023-07-14 17:28:48 +0900

    파일 복사 프로그램 풀이

---
+++ day20/src/api/file/single/Test03_2.java
@@ -0,0 +1,39 @@
+package api.file.single;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class Test03_2 {
+	public static void main(String[] args) throws IOException {
+		//파일 복사 프로그램
+		//준비물 : 입력용파일 + 스트림, 출력용파일 + 스트림
+		File a = new File("sample" , "copy.txt");	//절대경로(absolute path)
+		FileOutputStream stream = new FileOutputStream(a);
+		
+		File b = new File("D:/" , "origin.txt");	//상대경로(relative path)
+		FileInputStream stream2 = new FileInputStream(b);
+		
+		// -1이 들어가면 그만두도록 처리
+		long count = 0L;
+		long total = b.length();
+		DecimalFormat fmt = new DecimalFormat("#,##0.00");
+		
+
+		while(true) {
+		int f = stream2.read();
+			if(f == -1) break;
+			stream.write(f);
+			count++;
+			double percent = count * 100d / total;
+			System.out.println();
+
+			
+		}
+		stream.close();
+		stream2.close();
+ }
+
+}
