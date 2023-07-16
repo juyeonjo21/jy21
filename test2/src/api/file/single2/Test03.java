3b8253a99c2b050606267c6f5422996bae07229e
juyeonjo21 <merry2j@naver.com>
2023-07-16 18:11:10 +0900

    day20 과제 파일

---
+++ test2/src/api/file/single2/Test03.java
@@ -0,0 +1,39 @@
+package api.file.single2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		//기존에 만든 파일 복사 프로그램을 'byte[]'을 이용하여 처리하도록 변경
+		//최적의 버퍼 크기를 찾아서 설정	
+
+		byte[] buffer = new byte[1024];
+	
+		File  readTarget = new File("D:/" , "origin.txt"); 
+		FileInputStream stream = new FileInputStream(readTarget);
+				
+		File writeTarget = new File("sample" , "test03.txt");
+		FileOutputStream stream2 = new FileOutputStream(writeTarget);
+		
+		long start = System.currentTimeMillis();
+
+
+		while(true) {
+		int size = stream.read(buffer);
+		if(size == -1 ) break;
+		stream2.write(buffer, 0, size);
+		}
+		
+		long end = System.currentTimeMillis();		
+		long totalTime = end - start;
+	
+		System.out.println("실행시간 : " + totalTime + "ms" );
+		
+		stream.close();
+		stream2.close();
+		
+	}
+}
