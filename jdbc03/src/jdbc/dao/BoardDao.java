5d5db717971f9a45dea2722ce374cfee529823c2
juyeonjo21 <merry2j@naver.com>
2023-07-25 17:34:38 +0900

    게시글 조회 + TopN

--- jdbc03/src/jdbc/dao/BoardDao.java
+++ jdbc03/src/jdbc/dao/BoardDao.java
@@ -1,8 +1,11 @@
 package jdbc.dao;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import jdbc.dto.BoardDto;
+import jdbc.mapper.BoardMapper;
 import jdbc.util.JdbcUtils;
 //게시글을 CRUD 처리하는 도구
 public class BoardDao {
@@ -47,5 +50,32 @@ public class BoardDao {
 		
 		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
 		return jdbcTemplate.update(sql,data) > 0;
+	}
+	
+	//게시글 목록 전체 출력
+	private BoardMapper mapper = new BoardMapper();
+	
+	public List<BoardDto> selectList(){
+		String sql = "select * from board order by board_no desc";
+		
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		return jdbcTemplate.query(sql, mapper);
+	}
+	// 게시글 한페이지 당 10개씩 출력
+	//(심화) 페이징 목록 조회
+	public List<BoardDto> selectListByPage(int page) {
+		int end = page * 10;
+		int begin = end - 9;
+		
+		String sql = "select * from ("
+				+ "select rownum rn, TMP.* from("
+						+ "select * from board oder by board_no DESC" 
+						+	")TMP"
+						+") where rn between 1 and 10 ";
+		Object[] data = {begin, end};
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		return jdbcTemplate.query(sql, mapper, data);
+		
+		
 	}
 }
