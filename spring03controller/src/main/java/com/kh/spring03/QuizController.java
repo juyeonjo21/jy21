a2518876b0cde121a40189fc562c92aa09a76ae4
juyeonjo21 <merry2j@naver.com>
2023-07-26 14:57:44 +0900

    List 방법 추가

--- spring03controller/src/main/java/com/kh/spring03/QuizController.java
+++ spring03controller/src/main/java/com/kh/spring03/QuizController.java
@@ -19,5 +19,17 @@ public class QuizController {
 	int lotto = r.nextInt(45)+1;
 		return "로또 번호 : " + lotto;
 	}
+	//질문답변
+/*		@RequestMapping("/lotto-list")
+		public List<Integer> lottoList() {
+			List<Integer> list = new ArrayList<>();
+			for(int i=1; i <= 45;i ++) {
+				list.add(i);
+			}
+			Collections.shuffle(list);
+			List<Integer> lotto = list.subList(0, 6);
+			Collections.sort(lotto);
+			return lotto;
+		}*/
 }
 	 
