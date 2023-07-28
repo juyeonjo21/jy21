87d3b525a4035d255484dbcacc5f61cd49187cea
juyeonjo21 <merry2j@naver.com>
2023-07-28 10:25:28 +0900

    게시글 상세 컨트롤러+DAO+DTO

--- spring06board/src/main/java/com/kh/spring06/controller/BoardController.java
+++ spring06board/src/main/java/com/kh/spring06/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.kh.spring06.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring06.dao.BoardDao;
@@ -32,5 +35,49 @@ public class BoardController {
 			return "게시글이 존재하지 않습니다";
 		}
 	}
-
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int no) {
+		boolean result = dao.delete(no);
+		
+		if(result) {
+			return "게시글 삭제완료";
+		}
+		else {
+			return "존재하지 않는 게시글";
+		}
+	}
+	
+	@RequestMapping("/list")
+	public String list() {
+		List<BoardDto> list = dao.selectList();
+		StringBuffer buffer = new StringBuffer();//safe
+		//StringBuilder builder = new StringBuilder(); //non-safe
+		for(BoardDto dto : list) {
+			buffer.append(dto);
+			buffer.append(dto.getNo());
+			buffer.append(dto.getTitle());
+			buffer.append(dto.getWriter());
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	@RequestMapping("/detail")
+	public String detail(@RequestParam int no) {
+		BoardDto dto = dao.selectOne(no);
+	if(dto == null) {
+		return "존재하지 않는 게시글";
+	}
+	else {
+		//return dto.toString();
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("[제목]" + dto.getTitle());
+		buffer.append("(" + dto.getWriter() + ")");
+		buffer.append("<br>");
+		buffer.append("-------------------");
+		buffer.append("<br>");
+		buffer.append(dto.getContent());
+		buffer.append("<br>");
+		return buffer.toString();
+	}
+	}
 }
--- spring06board/src/main/java/com/kh/spring06/dao/BoardDao.java
+++ spring06board/src/main/java/com/kh/spring06/dao/BoardDao.java
@@ -1,15 +1,25 @@
 package com.kh.spring06.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.spring06.dto.BoardDto;
+import com.kh.spring06.mapper.BoardDetailMapper;
+import com.kh.spring06.mapper.BoardListMapper;
 
 @Repository
 public class BoardDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private BoardDetailMapper detailmapper;
+	
+	@Autowired
+	private BoardListMapper listmapper;
 	
 	public void insert(BoardDto dto) {
 		String sql = "insert into board(board_no, board_title,"
@@ -27,5 +37,24 @@ public class BoardDao {
 				dto.getTitle(), dto.getContent(), dto.getNo()
 		};
 		return jdbcTemplate.update(sql,data) > 0;
+	}
+	public boolean delete(int no) {
+		String sql = "delete board where board_no=?";
+		Object[] data = {no};
+		return jdbcTemplate.update(sql,data) > 0;
+	}
+	public List<BoardDto> selectList(){
+		String sql = "select "
+				+ "board_no, board_title, "
+				+ "board_writer, board_readcount "
+				+ "from "
+				+ "board order by board_no desc";
+		return jdbcTemplate.query(sql, listmapper);
+	}
+	public BoardDto selectOne(int no) {
+		String sql = "select * from board where board_no=?";
+		Object[] data = {no};
+		List<BoardDto> list = jdbcTemplate.query(sql, detailmapper, data);
+		return list.isEmpty() ? null : list.get(0);
 	}
 }
