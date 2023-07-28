896d4dd7597aa12b6896459a894f49a5c717db47
juyeonjo21 <merry2j@naver.com>
2023-07-28 10:05:25 +0900

    포켓몬 정보 등록 컨트롤러+DAO+Mapper

--- spring05jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
+++ spring05jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
@@ -1,8 +1,11 @@
 package com.kh.spring05.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring05.dao.PocketmonDao;
@@ -31,6 +34,39 @@ public class PocketmonController {
 		}
 		else {
 			return "해당 번호는 존재하지 않습니다";
+		}
+	}
+	//포켓몬스터 삭제(delete)
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int no) { //no 한개니까 파라미터
+		boolean result = dao.delete(no);
+		
+		if(result) {
+			return "삭제완료";
+		}
+		else {
+			return "존재하지 않는 포켓몬 번호";
+		}
+	}
+	@RequestMapping("/list")
+	public String list() {
+		List<PocketmonDto> list = dao.selectList();
+		StringBuffer buffer = new StringBuffer();
+		for(PocketmonDto dto : list) {
+			buffer.append(dto);
+			buffer.append("<br>");
+	}
+		return buffer.toString();		
+}
+	//상세조회 매핑
+	@RequestMapping("/detail")
+	public String detail(@RequestParam int no) {
+		PocketmonDto dto = dao.selectOne(no);
+		if(dto == null) {
+			return "존재하지 않는 포켓몬스터";
+		}
+		else {
+			return dto.toString();
 		}
 	}
 }
--- spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
+++ spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
@@ -1,10 +1,13 @@
 package com.kh.spring05.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.spring05.dto.PocketmonDto;
+import com.kh.spring05.mapper.PocketmonMapper;
 
 // DAO 클래스
 // = Spring에 등록해야됨
@@ -14,6 +17,9 @@ import com.kh.spring05.dto.PocketmonDto;
 public class PocketmonDao {
 	@Autowired//(주의)등록을 하지 않으면 절대로 주지 않음.
 	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired //mapper를 등록해주세요
+	private PocketmonMapper mapper;
 	
 	public void insert(PocketmonDto dto) {
 		String sql = "insert into pocketmon(no,name,type)"
@@ -31,5 +37,22 @@ public class PocketmonDao {
 				dto.getName(), dto.getType(), dto.getNo()
 		};
 		return jdbcTemplate.update(sql,data) > 0;
+	}
+	public boolean delete(int no) {
+		String sql = "delete pocketmon where no=?";
+		Object[] data = {no};
+		return jdbcTemplate.update(sql,data) > 0;
+	}
+	//기본 조회
+	public List<PocketmonDto> selectList(){
+		String sql = "select * from pocketmon order by no asc";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	//단일조회(상세)
+	public PocketmonDto selectOne(int no) {
+		String sql = "select * from pocketmon where no = ?";
+		Object[]data = {no};
+		List<PocketmonDto> list = jdbcTemplate.query(sql,mapper,data);
+		return list.isEmpty() ? null : list.get(0);
 	}
 }
