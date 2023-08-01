d482c0ff3985c77bac776cb9da80aadfae54026b
juyeonjo21 <merry2j@naver.com>
2023-08-01 13:46:10 +0900

    구내식당 table 생성 문제

--- spring09view/src/main/java/com/kh/spring09/controller/TableController.java
+++ spring09view/src/main/java/com/kh/spring09/controller/TableController.java
@@ -10,4 +10,8 @@ public class TableController {
 	public String test01() {
 		return "/WEB-INF/views/table/test01.jsp";
 	}
+	@RequestMapping("/test02")
+	public String test02() {
+		return "/WEB-INF/views/table/test02.jsp";
+	}
 }
