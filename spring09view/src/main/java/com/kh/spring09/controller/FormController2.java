c337218bccef8666084a30cb1b41deb27dcf9766
juyeonjo21 <merry2j@naver.com>
2023-08-01 12:41:59 +0900

    회원탈퇴 페이지 구현

--- spring09view/src/main/java/com/kh/spring09/controller/FormController2.java
+++ spring09view/src/main/java/com/kh/spring09/controller/FormController2.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/form2")
 public class FormController2 {
 	@RequestMapping("/test01")
-	public String terst01() {
+	public String test01() {
 		return "/WEB-INF/views/form2/test01.jsp";
+	}
+	@RequestMapping("/test02")
+	public String test02() {
+		return "/WEB-INF/views/form2/test02.jsp";
+	}
+	@RequestMapping("/test03")
+	public String test03() {
+		return "/WEB-INF/views/form2/test03.jsp";
 	}
 }
