711b542d9c6c0a8b2268435f43bf71e67434d0e3
juyeonjo21 <merry2j@naver.com>
2023-08-07 14:16:52 +0900

    프로퍼티즈 코드 수정

---
+++ testhome/src/test/java/com/example/demo/TesthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
