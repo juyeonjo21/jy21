b0185e1967da18ed2a0f07b15370c1705ac626e6
juyeonjo21 <merry2j@naver.com>
2023-08-07 19:16:44 +0900

    spring home 초기 설정

---
+++ springhome/src/main/webapp/WEB-INF/views/template/footer.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+
+    <hr>
+    KH정보교육원 웹개발자 양성과정
---
+++ springhome/src/test/java/com/springhome/SpringhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
