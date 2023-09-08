29f945f278091afcd7b012fb96127085b2c6ee9b
juyeonjo21 <merry2j@naver.com>
2023-09-08 16:12:04 +0900

    회원가입 아이디 중복검사 추가(백엔드)

--- springhome/src/main/java/com/kh/springhome/dao/MemberDao.java
+++ springhome/src/main/java/com/kh/springhome/dao/MemberDao.java
@@ -27,10 +27,13 @@ public interface MemberDao {
 	
     List<MemberBlockDto> selectBlockList();
     MemberBlockDto selectBlockOne(String memberId);
+    
 	
 	//포인트 추가
 //	boolean updateMemberPoint(String memberId);//조회수의 경우엔 무조건 1씩 증가이므로 쓸 수 있지만
 	//각기 다른 값이 추가될 가능성이 있는 경우 아래처럼 쓴다
 	boolean increaseMemberPoint(String memberId, int point);
 //	boolean decreaseMemberPoint(String memberId, int point);
+	
+	MemberDto selectOneByNickname(String memberNickname);
 	}
