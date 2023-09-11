29294a0c619f151f9ecb3a332561d269eef6e535
juyeonjo21 <merry2j@naver.com>
2023-09-11 11:12:36 +0900

    백엔드 구현

---
+++ spring11.ajax/src/main/java/com/kh/spring11/dto/PocketmonDto.java
@@ -0,0 +1,39 @@
+package com.kh.spring11.dto;
+
+public class PocketmonDto {
+	private int no;
+	private String name;
+	private String type;
+	
+	// POJO클래스
+	// = Plain Old Java Object
+	// - Spring에 등록하지 않아도 됨
+	public PocketmonDto() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "PocketmonDto [no=" + no + ", name=" + name + ", type=" + type + "]";
+	}
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+
+}
