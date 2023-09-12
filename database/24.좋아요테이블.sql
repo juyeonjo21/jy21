b0c6bea49c65f7a58f0b8706e89649b7318d4fb3
juyeonjo21 <137031318+juyeonjo21@users.noreply.github.com>
2023-09-12 16:17:05 +0900

    Create 24.좋아요테이블.sql

---
+++ database/24.좋아요테이블.sql
@@ -0,0 +1,18 @@
+CREATE TABLE board_like(
+member_id REFERENCES member(member_id) ON DELETE CASCADE NOT NULL,
+board_no REFERENCES board(board_no) ON DELETE CASCADE NOT NULL,
+PRIMARY KEY(member_id,board_no)
+);
+
+SELECT * FROM board_like;
+
+
+
+CREATE TABLE follow(
+follower REFERENCES member(member_id) ON DELETE CASCADE NOT NULL,
+target REFERENCES member(member_id) ON DELETE CASCADE NOT NULL,
+follow_time DATE DEFAULT sysdate NOT NULL,
+PRIMARY KEY(follower, target)
+);
+
+SELECT * FROM follow;
