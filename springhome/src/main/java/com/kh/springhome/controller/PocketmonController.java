2aea992f83a214c91d31dd9b0eb5108077e85551
juyeonjo21 <merry2j@naver.com>
2023-09-14 13:55:33 +0900

    파일 다운로드 처리(포켓몬 이미지)

--- springhome/src/main/java/com/kh/springhome/controller/PocketmonController.java
+++ springhome/src/main/java/com/kh/springhome/controller/PocketmonController.java
@@ -3,19 +3,24 @@ package com.kh.springhome.controller;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.springhome.dao.AttachDao;
 import com.kh.springhome.dao.PocketmonDao;
 import com.kh.springhome.dto.AttachDto;
 import com.kh.springhome.dto.PocketmonDto;
+import com.kh.springhome.error.NoTargetException;
 
 @Controller
 @RequestMapping("/pocketmon")
@@ -70,5 +75,46 @@ public class PocketmonController {
 			return "redirect:insert";
 		
 	}
-
+	
+	//파일 다운로드
+	//- 파일번호(attach_no)를 이용하여 정보를 조회
+	//- 실물 파일을 불러와서 사용자에게 전송
+	//- 화면이 아니라 파일을 전송해야 하므로 RestController처럼 처리
+	//- 매핑 위에 @ResponseBody 라고 추가하면 가능
+	@ResponseBody
+	@RequestMapping("/image")
+	public ResponseEntity<ByteArrayResource> 
+							image(@RequestParam int no) throws IOException {
+		//[1] 포켓몬 번호로 파일 번호를 찾아야 한다
+		//[2] 파일 번호로 파일 정보를 불러와야 한다
+		//[3] 실제 파일을 불러와야 한다
+		//[4] 앞에서 불러온 모든 정보로 ResponseEntity를 생성한다
+		//[5] 사용자한테 준다
+		
+		//1,2
+		AttachDto attachDto = pocketmonDao.findImage(no);
+		if(attachDto == null) {
+			//throw new NoTargetException("파일 없음");//내가만든 예외로 통합
+			return ResponseEntity.notFound().build();//404 반환
+		}
+		
+		//3
+		String home = System.getProperty("user.home");
+		File dir = new File(home, "upload");
+		File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
+		
+		byte[] data = FileUtils.readFileToByteArray(target);//실제파일정보 불러오기
+		ByteArrayResource resource = new ByteArrayResource(data);
+		
+		
+		//4,5 - header(정보), body(내용)
+		return ResponseEntity.ok()
+				.header("Content-Encoding","UTF-8")
+				.header("Content-Length",String.valueOf(attachDto.getAttachSize()))
+//				.header("Content-Type",attachDto.getAttachType())//저장된유형
+				.header("Content-Type","application/octet-stream")//무조건 다운로드
+				.header("Content-Disposition","attachment; filename="+attachDto.getAttachName())
+				.body(resource);
+	}
+	
 }
--- springhome/src/main/java/com/kh/springhome/dao/PocketmonDaoImpl.java
+++ springhome/src/main/java/com/kh/springhome/dao/PocketmonDaoImpl.java
@@ -1,10 +1,14 @@
 package com.kh.springhome.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.springhome.dto.AttachDto;
 import com.kh.springhome.dto.PocketmonDto;
+import com.kh.springhome.mapper.AttachMapper;
 import com.kh.springhome.mapper.PocketmonMapper;
 
 @Repository
@@ -15,6 +19,9 @@ public class PocketmonDaoImpl implements PocketmonDao{
 	
 	@Autowired
 	private PocketmonMapper pocketmonMapper;
+	
+	@Autowired
+	private AttachMapper attachMapper;
 
 	 @Override
 	   public int sequence() {
@@ -35,5 +42,17 @@ public class PocketmonDaoImpl implements PocketmonDao{
 		Object[] data = {pocketmonNo, attachNo};
 		jdbcTemplate.update(sql,data);
 		
+	}
+
+	@Override
+	public AttachDto findImage(int pocketmonNo) {
+		String sql = "select * from attach "
+				+ "where attach_no = ("
+				+ "select attach_no from pocketmon_image "
+				+ "where pocketmon_no = ?"
+				+ ")";
+		Object[] data = {pocketmonNo};
+		List<AttachDto> list = jdbcTemplate.query(sql, attachMapper, data);
+		return list.isEmpty() ? null : list.get(0);
 	}
 }
