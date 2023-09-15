5c419e4523f47b53ef87708eb2a3e6e98155cd46
juyeonjo21 <merry2j@naver.com>
2023-09-15 10:56:40 +0900

    파일 경로 커스터마이징 설정

--- spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
+++ spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.spring12.configuration.FileUploadProperties;
 import com.kh.spring12.dao.AttachDao;
 import com.kh.spring12.dto.AttachDto;
 
@@ -30,6 +33,19 @@ public class FileRestController {
 	@Autowired
 	private AttachDao attachDao;
 	
+	//미리 작성해둔 커스텀 속성을 불러와서 디렉터리 객체까지 생성
+	@Autowired
+	private FileUploadProperties props;
+	
+	private File dir;
+	
+	@PostConstruct//모든 로딩이 끝나면 자동으로 실행되는 메소드
+	public void init() {
+		dir = new File(props.getHome());
+		dir.mkdirs();
+		
+	}
+	
 	//비동기 통신에서는 화면에서 다음 작업이 가능하도록 파일번호 등을 전달해야한다
 	@PostMapping("/upload")
 	public Map<String, Object> upload(@RequestParam MultipartFile attach) throws IllegalStateException, IOException{
@@ -41,11 +57,6 @@ public class FileRestController {
 		
 		//[2]시퀀스 번호를 파일명으로 하여 실제 파일을 저장한다
 		//- 같은 이름에 대한 충돌을 방지하기 위해
-		String home = System.getProperty("user.home");
-		File dir = new File(home, "upload");//저장할 디렉터리
-		dir.mkdirs(); //디렉터리 생성
-		
-		
 		File target = new File(dir,String.valueOf(attachNo)); //저장할 파일
 		attach.transferTo(target);//저장
 		
@@ -59,7 +70,8 @@ public class FileRestController {
 
 		//화면에서 사용할 수 있도록 파일번호 또는 다운주소를 반환
 //		return 객체 or Map;
-		return Map.of("attachNo",attachNo); //-> JSON으로 알아서 변환해줌
+		return Map.of("attachNo",attachNo); //-> JSON으로 알아서 반환해줌(class/Map)
+											//sts에서 JSON을 반환해주는 라이브러리 = Jackson-databind
 	}
 
 	//다운로드는 비동기나 동기나 똑같음(파일번호만 알면 됨)
@@ -72,13 +84,12 @@ public class FileRestController {
 			//throw new NoTargetException("파일 없음");//내가만든 예외로 통합
 			return ResponseEntity.notFound().build();//404 반환
 		}
-			String home = System.getProperty("user.home");
-			File dir = new File(home, "upload");
 			File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
-			System.out.println(attachDto);
+
 			
 			byte[] data = FileUtils.readFileToByteArray(target);//실제파일정보 불러오기
 			ByteArrayResource resource = new ByteArrayResource(data);
+			
 			
 			return ResponseEntity.ok()
 					.header(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name())
