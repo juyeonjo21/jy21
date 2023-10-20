afde8c8c67922fdeb0d2251cb325953bef217aaf
juyeonjo21 <merry2j@naver.com>
2023-10-20 15:56:38 +0900

    비회원은 채팅을 전달하지 않도록 처리

--- spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
+++ spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
@@ -1,5 +1,6 @@
 package com.kh.spring20.websocket;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -23,6 +24,10 @@ public class MemberWebSocketServer extends TextWebSocketHandler {
 		log.debug("session = {}", session.getAttributes());
 		
 		//session의 추가 정보(attributes)를 조사하여 HttpSession의 정보를 추출하여 사용
+		Map<String, Object> attr = session.getAttributes();
+		String memberId = (String) attr.get("name");
+		String memberLevel = (String) attr.get("level");
+		log.debug("아이디 = {}, 등급 = {}", memberId, memberLevel);
 		
 		log.debug("사용자 접속 = {}", clients.size());
 	}
@@ -34,6 +39,14 @@ public class MemberWebSocketServer extends TextWebSocketHandler {
 	}
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		//기존 HTTp 세션의 정보를 조회
+		Map<String, Object> attr = session.getAttributes();
+		String memberId = (String) attr.get("name");
+		String memberLevel = (String) attr.get("level");
+		if(memberId == null || memberLevel == null) {//비회원이라면
+			return;
+		}
+		
 		for(WebSocketSession client:clients) {
 			client.sendMessage(message);
 		}
