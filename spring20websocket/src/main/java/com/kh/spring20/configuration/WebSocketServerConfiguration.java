74934e719a47ebca253d18a8c492ab1e2d429fcf
juyeonjo21 <merry2j@naver.com>
2023-10-20 14:29:26 +0900

    회원 정보를 전달하는 웹소켓 서버 구현 및 등록

--- spring20websocket/src/main/java/com/kh/spring20/configuration/WebSocketServerConfiguration.java
+++ spring20websocket/src/main/java/com/kh/spring20/configuration/WebSocketServerConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import com.kh.spring20.websocket.DefaultWebSocketServer;
 import com.kh.spring20.websocket.GroupWebSocketServer;
+import com.kh.spring20.websocket.MemberWebSocketServer;
 import com.kh.spring20.websocket.TimeWebSocketServer;
 
 //이 클래스는 생성한 웹소켓 서버를 어떤 주소에 할당하도록 설정하는 역할을 한다
@@ -23,6 +25,9 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer{
 	
 	@Autowired
 	private GroupWebSocketServer groupWebSocketServer;
+	
+	@Autowired
+	private MemberWebSocketServer memberWebSocketServer;
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -31,6 +36,11 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer{
 		registry.addHandler(defaultWebSocketServer, "/ws/default");
 		registry.addHandler(timeWebSocketServer, "/ws/time");
 		registry.addHandler(groupWebSocketServer, "/ws/group");
+		
+		//아래와 같이 등록하면 HttpSession의 정보를 WebSocketSession으로 옮겨준다
+		registry.addHandler(memberWebSocketServer, "/ws/member")
+				.addInterceptors(new HttpSessionHandshakeInterceptor());
+		
 	}
 	
 	
