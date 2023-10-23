584cf5a664faed60dd4c119f09f6cacf96d969c3
juyeonjo21 <merry2j@naver.com>
2023-10-23 13:41:48 +0900

    회원목록과 메세지 모두 처리하는 웹소켓 예제

--- spring20websocket/src/main/java/com/kh/spring20/websocket/SockJsWebSocketServer.java
+++ spring20websocket/src/main/java/com/kh/spring20/websocket/SockJsWebSocketServer.java
@@ -20,54 +20,86 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class SockJsWebSocketServer extends TextWebSocketHandler {
-	
-	//저장소
-	//private Set<WebSocketSession> clients = new CopyOnWriteArraySet<>();
-	private Set<ClientVO> clients = new CopyOnWriteArraySet<>();
-	
-	
+
+	// 저장소
+	// private Set<WebSocketSession> clients = new CopyOnWriteArraySet<>();
+	private Set<ClientVO> clients = new CopyOnWriteArraySet<>();// 전체회원
+	private Set<ClientVO> members = new CopyOnWriteArraySet<>();// 로그인한 회원
+
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		ClientVO client = new ClientVO(session); //생성자 만들어 뒀으니 여기서 씀
+		ClientVO client = new ClientVO(session); // 생성자 만들어 뒀으니 여기서 씀
 		clients.add(client);
-		//clients.add(session);
+
+		if (client.isMember()) { // 회원이라면
+			members.add(client);// 추가
+
+		}
+
+		// clients.add(session);
 		log.debug("사용자 접속. 현재 {}명", clients.size());
 		log.debug("접속한 사용자 ={}", client);
-		
-		//모든 접속자에게 접속자 명단을 전송
-		//메소드 추가
+
+		// 모든 접속자에게 접속자 명단을 전송
+		// 메소드 추가
 		sendClientList();
 	}
-	
+
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		ClientVO client = new ClientVO(session);
 		clients.remove(client);
-		//clients.remove(session);
+
+		if (client.isMember()) {
+			members.remove(client);
+
+		}
+
+		// clients.remove(session);
 		log.debug("사용자 접속 종료! 현재 {}명", clients.size());
-		log.debug("종료한 사용자 ={}", client);
-		
-		//모든 접속자에게 접속자 명단을 전송
-		//메소드 추가
+
+		// 모든 접속자에게 접속자 명단을 전송
+		// 메소드 추가
 		sendClientList();
 	}
-	
-	//접속자 명단(clients)을 모든 접속자에게 전송하는 메소드
+
+	// 접속자 명단(clients)을 모든 접속자에게 전송하는 메소드
 	public void sendClientList() throws IOException {
-		//1. clients를 전송 가능한 형태(JSON 문자열)로 변환
+		// 1. clients를 전송 가능한 형태(JSON 문자열)로 변환
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		Map<String, Object> data = new HashMap<>();
-		data.put("clients", clients);
+		// data.put("clients", clients);//전체회원 명단(null이 문제가 됨)
+		data.put("clients", members);// 로그인한 회원명단
 		String clientJson = mapper.writeValueAsString(data);
-		
-		//2.모든 사용자에게 전송
+
+		// 2.모든 사용자에게 전송
 		TextMessage message = new TextMessage(clientJson);
-		for(ClientVO client:clients) {
+		for (ClientVO client : clients) {
 			client.send(message);
 		}
 	}
-	
-	
 
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		// 사용자가 보낸 메세지를 모두에게 broadcast (회원이 아닌 전체에게 전송)
+		ClientVO client = new ClientVO(session);
+		if (client.isMember() == false)
+			return; // 멤버가 아니면 메세지 못 봄
+
+		// 정보를 Map에 담아 변환 후 전송
+		Map<String, Object> map = new HashMap<>();
+		map.put("memberId", client.getMemberId());
+		map.put("memberLevel", client.getMemberLevel());
+		map.put("content", message.getPayload());
+		// 시간 추가 등
+
+		ObjectMapper mapper = new ObjectMapper();
+		String messageJson = mapper.writeValueAsString(map);
+		TextMessage tm = new TextMessage(messageJson);
+
+		for (ClientVO c : clients) {
+			c.send(tm);
+		}
+	}
 }
--- spring20websocket/src/main/webapp/WEB-INF/views/sockjs.jsp
+++ spring20websocket/src/main/webapp/WEB-INF/views/sockjs.jsp
@@ -3,6 +3,11 @@
 
     <h1>SockJS를 적용한 웹소켓 예제</h1>
     
+    <input type="text" class="message-input">
+    <button type="button" class="send-btn">전송</button>
+    
+    <div class="client-list"></div>
+    <div class="message-list"> </div>
     
     
     
@@ -19,8 +24,38 @@
      window.socket = new SockJS("${pageContext.request.contextPath}/ws/sockjs"); //http로 시작하는 주소
      //연결 후 해야 할 일들을 콜백함수로 지정(onopen, onclose, onerror, onmessage)
      window.socket.onmessage = function(e){
-    	 console.log(e);
+    	 //console.log(e);
+    	 var data = JSON.parse(e.data);
+    	 //console.log(data);
+    	 
+    	 //사용자가 접속하거나 종료했을 때 서버에서 오는 데이터로 목록을 갱신
+    	 //사용자가 메세지를 보냈을 때 서버에서 이를 전체에게 전달한다
+    	 //data.clents 에 회원 목록이 있다
+    	 if(data.clients){ //true냐(있냐) - 목록 처리
+    	 $(".client-list").empty();
+    	 for(var i=0; i < data.clients.length; i++){
+    		 $("<div>").text(data.clients[i].memberId).appendTo(".client-list");  		 
+    	 }
+    	}
+    	 else if(data.content){//메세지 처리
+    		var memberId = $("<div>").text(data.memberId);
+     		var memberLevel = $("<div>").text(data.memberLevel);
+     		var content = $("<div>").text(data.content);
+     		
+     		$("<div>").append(memberId)
+     				  .append(memberLevel)
+     				  .append(content)
+     				  .appendTo(".message-list"); 
+    	}
      };
+     
+     $(".send-btn").click(function(){
+    	 var text = $(".message-input").val();
+    	 if(text.length == 0) return;
+    	 
+    	 window.socket.send(text);
+    	 $(".messgae-input").val("");
+     });
      </script>
      
      
