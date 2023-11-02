fa4a96286c836b5ca791e1142440a2f7dcecf6c9
juyeonjo21 <merry2j@naver.com>
2023-11-02 18:13:21 +0900

    rest api 기본 세팅

---
+++ spring22rest/src/main/java/com/kh/spring22/dto/PocketmonDto.java
@@ -0,0 +1,14 @@
+package com.kh.spring22.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class PocketmonDto {
+	private int no;
+	private String name, type;
+
+}
