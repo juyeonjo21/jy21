da3cddb64ae3e35af8b3c599851d4a04fd9f8fef
juyeonjo21 <merry2j@naver.com>
2023-11-29 17:36:22 +0900

    spring boot 3.0 설정

---
+++ spring3x/src/main/java/com/kh/spring3x/Spring3xApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring3x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3xApplication.class, args);
+	}
+
+}
